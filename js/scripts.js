04725db7b7861d34b5d8304054e22a450fb8d234
BenardBett <kiplangatbett5@gmail.com>
2020-10-21 12:01:46 +0300

    modified js file

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function ben(){
+    alert("hello")
+     if(id01.target === submit) {
+       return pwd
+        
+    }
+}
